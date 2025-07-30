0e924096100116dff2b238ad741981eb9291b7cc
Mitt solanki <solankimitesh210@gmail.com>
2025-07-31 01:11:16 +0530

    completely changed structure

---
+++ serverside/routes/auth.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const dotenv = require('dotenv')
+const JWT_SECRET = process.env.JWT_SECRET
+
+router.post('/signup',async (req,res)=>{
+    const {username, email, password} = req.body
+    if(!username || !email || !password){
+        return res.status(400).json({success:false , message :'all fields are required'})
+    }
+    try{
+        const existingUser = await User.findOne({email})
+        if(existingUser)
+          return res.status(400).json({success : false ,message:'user already exists '})
+        const hashedPwd = await bcrypt.hash(password,10)
+        const newUser = new User({username,email,password:hashedPwd})
+        await newUser.save()
+
+        const token = jwt.sign({userid:newUser._id},JWT_SECRET,{expiresIn : '1h'})
+        res.status(201).json({success : true,token})
+    }
+    catch (error) {
+        console.error('API Error:', error); // Add this line
+        console.error('Error details:', error.message); // And this
+        console.error('Stack trace:', error.stack)
+         res.status(500).json({success:false, message:'internal server error'})
+    }
+    
+})
+
+
+
+router.post('/login',async (req,res)=>{
+    const {email,password} = req.body
+
+    try{
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({success:false,message:'user does not exist'})
+        }
+        const isMatch = await bcrypt.compare(password,user.password)
+        if(!isMatch){
+            return res.status(400).json({success:false, message:'invalid credintials '})
+        }
+        const token = jwt.sign({userid : user._id},JWT_SECRET,{expiresIn:'1h'})
+        return res.status(200).json({success:true , token})
+    }
+    catch(err){
+        console.error(err)
+        res.status(500).json({err:'internal server error'})
+    }
+})
+
+module.exports = router
---
+++ serverside/server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const authRoutes = require('./routes/auth')
+const dashBoard = require('./routes/dashboard')
+const path = require('path')
+
+dotenv.config({path: path.join(__dirname ,'../.env')})
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/auth',authRoutes)
+app.use('/dashboard',dashBoard)
+app.use(express.static(path.join(__dirname, '../Client_side')))
+
+app.get('/',(req,res) =>{
+    res.sendFile(path.join(__dirname,'../Client_side/index.html'))
+})
+
+const mongUri = process.env.MONGO_URL
+
+if(!mongUri){
+    console.error('mongo url not found')
+    // process.exit(1)
+}
+
+
+mongoose.connect(mongUri)
+.then(()=> console.log('MongoDB connection established'))
+.catch((err)=> console.error('MongoDB connection error:', err))
+
+const PORT = process.env.PORT 
+
+app.listen(PORT , ()=>{
+    console.log(`server is running at http://localhost:${PORT}`)
+})
