6051f682532008c9dee4aad49396a1d64eb6ff8b
Mitt solanki <solankimitesh210@gmail.com>
2025-07-30 20:07:25 +0530

    changed the whole structure

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-const authRoutes = require('./routes/auth')
-const dashBoard = require('./routes/dashboard')
+const authRoutes = require('./frontend/backend/routes/auth')
+const dashBoard = require('./frontend/backend/routes/dashboard')
 const path = require('path')
 
 dotenv.config()
@@ -19,11 +19,19 @@ app.get('/',(req,res) =>{
     res.sendFile(path.join(__dirname,'..','index.html'))
 })
 
-mongoose.connect(process.env.MONGO_URL)
+const mongUri = process.env.MONGO_URL
+
+if(!mongUri){
+    console.error('mongo url not found')
+    // process.exit(1)
+}
+
+
+mongoose.connect(mongUri)
 .then(()=> console.log('MongoDB connection established'))
 .catch((err)=> console.error('MongoDB connection error:', err))
 
-const PORT = process.env.PORT || 5005
+const PORT = process.env.PORT 
 
 app.listen(PORT , ()=>{
     console.log(`server is running at http://localhost:${PORT}`)
