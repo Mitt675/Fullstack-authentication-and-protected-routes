402405fb6f9cf135252154d42a5115f7d856ef18
Mitt solanki <solankimitesh210@gmail.com>
2025-07-30 16:05:09 +0530

    signup completetion

--- frontend/signup.js
+++ frontend/signup.js
@@ -1,21 +1,22 @@
 const { json } = require("express");
 
-const signupform = document.getElementsByClassName('signup-form').addEventlistner('submit',async (e)=>{
+const signupform = document.getElementsByClassName('signupForm').addEventlistner('submit',async (e)=>{
     e.preventDefauld();
 
     const username = document.getElementById('userName').value 
     const email = document.getElementById('email').value
     const password = document.getElementById('password').value
-
+try{
+  
     const res = await fetch('http://localhost:5005/api/auth/signup',{
       method : 'POST',
-      headers : {'content-Type':'application/json'},
-      body : json.stringfy({username,email,password})
+      headers : {'Content-Type':'application/json'},
+      body : JSON.stringify({username,email,password})
 
 
     })
     const data = await res.json()
-    try{
+    
        if(data.success){
       localStorage.setItem('token',data.token)
       message.textContent = 'signup successfull'
@@ -26,7 +27,8 @@ const signupform = document.getElementsByClassName('signup-form').addEventlistne
       message.textContent = 'signup is failed'
       message.style.color = 'red'
     }
-    }
+    
+}
     
     catch(err){
       message.textContent = 'internla server error'
